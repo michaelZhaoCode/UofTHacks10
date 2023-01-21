60d1332740e5215423cf99889f02bad2c9ca8eb8
michaelZhaoCode <82855036+michaelZhaoCode@users.noreply.github.com>
2023-01-21 01:35:16 -0500

    sql updates

--- sql.py
+++ sql.py
@@ -1,3 +1,42 @@
 import sqlite3
 
 conn = sqlite3.connect("mydatabase.db")
+mycursor = conn.cursor()
+
+def create_messages_table():
+    """Initialize message table"""
+
+    command = f"CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, user INTEGER, message nvarchar(100))"
+    mycursor.execute(command)
+    conn.commit()
+
+def insert_message(user: int, message:str):
+    """Insert new message into messages"""
+    command = f"INSERT INTO messages (user, message) VALUES ('{user}', '{message}')"
+    mycursor.execute(command)
+    conn.commit()
+
+
+def remove_user(user: id):
+    """Remove all messages from user"""
+    command = f"DELETE FROM messages WHERE user = {user}"
+    mycursor.execute(command)
+    conn.commit()
+
+
+def remove_message(user: id, message: str):
+    """Remove message from user"""
+    command = f"DELETE FROM messages WHERE user = {user} AND message = '{message}'"
+    mycursor.execute(command)
+    conn.commit()
+
+def remove_specific(message_id: id):
+    """Remove particular message from user"""
+    command = f"DELETE FROM messages WHERE id = {message_id}"
+    mycursor.execute(command)
+    conn.commit()
+
+
+# create_messages_table()
+# mycursor.execute('SELECT * FROM messages')
+# print(mycursor.fetchall())
